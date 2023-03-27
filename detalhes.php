4010bcf53efae4fab6e5821f57c04b8d42a82877
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2023-03-27 16:04:40 -0300

    PHP com MySQL: Atualização do título

--- detalhes.php
+++ detalhes.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<title> Título da Página</title>
+	<title>Detalhes do Jogo</title>
 	<meta charset="UTF-8"/>
 	<link rel="stylesheet" href="estilos/estilo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -9,6 +9,7 @@
 <body>
     <?php
         require_once "includes/banco.php";
+        require_once "includes/login.php";
         require_once "includes/funcoes.php";
     ?>
 	<div id="corpo">
