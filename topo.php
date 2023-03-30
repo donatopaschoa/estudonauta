50d1cb333a5859fdaeb967877cc873b9f823386b
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2023-03-30 17:44:33 -0300

    PHP com MySQL: Inclusão link Meus Dados

--- topo.php
+++ topo.php
@@ -6,7 +6,7 @@
         echo "<a href='user-login.php'>Entrar</a>";
     }else{
         echo "Olá, <strong>". $_SESSION['nome'] ."</strong> (". $_SESSION['tipo'] .") | ";
-        echo "Meus Dados | ";
+        echo "<a href='user-edit.php'>Meus Dados</a> | ";
 
         if(is_admin()){
             echo "<a href='user-new.php'> Novo usuário</a> | ";
