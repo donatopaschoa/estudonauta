4bdefd089a46e9ffbfe8fe9d88b48e77970b0d0e
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2023-04-10 09:15:44 -0300

    PHP com MySQL: Senha expirada e históric no cadatr
    Implementação de verif. de senha duplicada no banco de dados e cadastro do histórico das atividades do usuário cadastrante p/ identificar o responsável

--- user-edit.php
+++ user-edit.php
@@ -17,7 +17,7 @@
         <div id="corpo">
             <?php
                 // se não tiver logado 
-                // (qdo um esperto q não tiver logado tentar acessar diretamente "user-edit.php"):
+                // (qdo um esperto q não tiver logado e tentar acessar diretamente "user-edit.php"):
                 if(!is_logado()){
                     echo msg_erro("Efetue o <a href='user-login.php'>login</a> para editar seus dados");
                 }else{
@@ -36,17 +36,59 @@
                         /* // teste:
                         echo msg_sucesso("Dados foram recebidos: <br>Usuário: $usuario<br>Nome: $nome<br>Tipo: $tipo<br>Senha1: $senha1<br>Senha2: $senha2<br>"); */
 
+                        // implementando: 
+                        // - verificar se a nova senha é igual a senha anterior
+                        // - inserir atividades realizadas pelo usuário
+                        $qs = "select usuario, nome, senha, tipo, dtStatus, dtSenha, atividade from usuarios where usuario = '$usuario'";
+                        $busca = $banco->query($qs);
+                        $reg = $busca->fetch_object();
+                        
+                        // coletando dados antigos (antes de atualizar):
+                        $usuarioAntigo = $reg->usuario;
+                        $nomeAntigo = $reg->nome;
+                        $senhaAntigo = $reg->senha;
+                        $tipoAntigo = $reg->tipo;
+                        $dtStatusAntigo = $reg->dtStatus;
+                        $dtSenhaAntigo = $reg->dtSenha;
+                        $atividadeAntigo = $reg->atividade;
+                        
+                        $dados = "<hr>Dados cadastrados na base de $usuario: <br>";
+                        $dados .= "Usuário: $usuarioAntigo <br>";
+                        $dados .= "Nome: $nomeAntigo <br>";
+                        $dados .= "Senha: $senhaAntigo <br>";
+                        $dados .= "Tipo: $tipoAntigo <br>";
+                        $dados .= "dtStatus: $dtStatusAntigo <br>";
+                        $dados .= "dtSenha: $dtSenhaAntigo <br>";
+                        $dados .= "$atividadeAntigo <br>";
+
+
                         // salvando somente o nome e a senha (usuario e tipo não permite alteração):
                         $q = "update usuarios set ";
                         $q .= "nome = '$nome' ";
+                        $q .= ", dtStatus = now() ";
+                        $q .= ", atividade = '$dados'";
 
                         // campo senha1 e senha2 não foram preenchidas:
                         if( (empty($senha1) || is_null($senha1)) && (empty($senha2) || is_null($senha2)) ){
-                            echo msg_aviso("Sua senha antiga foi mantida");
+                            if ($_SESSION['senhaExpirada'] == false){
+                                echo msg_aviso("Sua senha antiga foi mantida");
+                                }else{
+                                echo msg_aviso("Sua senha antiga foi mantida mas está EXPIRADA, favor fazer login e alterar para uma senha DIFERENTE");
+                            }
+                            
                         }else{
                             // os campos das senhas foram preenchidos:
                             if($senha1 === $senha2){
-                                $q .= ", senha = '". gerarHash($senha1) . "'";
+                                // implementação: consulta se a senha digitada é igual a antiga e atualiza a dtSenha:
+                                if(testarHash($senha1, $senhaAntigo)){
+                                    echo msg_erro("Favor digitar outra senha pois você está repetindo a senha antiga (fazer login pela senha antiga e recadastrar novamente)");
+                                }else{
+                                    $q .= ", senha = '". gerarHash($senha1) . "'";
+                                    $q .= ", dtSenha = now() ";
+
+                                    // a senha foi alterada, deixou de expirar:
+                                    $_SESSION['senhaExpirada'] = false;
+                                }
                             }else{
                                 // o preenchimento das senhas estão diferentes:
                                 echo msg_aviso("As senhas não conferem!<br>A senha anterior será mantida");
@@ -67,8 +109,9 @@
                         }
                     }
                 }
-
-                echo iconeVoltar();
+                if(($_SESSION['senhaExpirada'] == false)){ // se a senha NÃo estiver expirada, o link será apresentado
+                    echo iconeVoltar();
+                }
             ?>
         </div>
         <?php include_once "rodape.php"; // rodapé com fechamento do banco de dados ?>
