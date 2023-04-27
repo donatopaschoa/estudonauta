01e8231208ab3508dc9738dd96b459e12316dbbf
Donato Paschoa Amezaga <donato.amezaga@gmail.com>
2023-04-27 16:01:46 -0300

    PHP com MySQL: Visibilidade do menu
    Permissão de visibilidade do menu conforme tipo de usuário (editor ou adm):

--- topo.php
+++ topo.php
@@ -6,15 +6,19 @@
         echo "<a href='user-login.php'>Entrar</a>";
     }else{
         echo "Olá, <strong>". $_SESSION['nome'] ."</strong> (". $_SESSION['tipo'] .") | ";
-        echo "<a href='user-edit.php'>Meus Dados</a> | ";
+        echo "<a href='user-edit.php'>Meus Dados</a> ";
 
         if(is_admin() && ($_SESSION['senhaExpirada'] == false)){
-            echo "<a href='user-new.php'> Novo usuário</a> | ";
-            echo "Novo jogo | ";
-            echo "<a href='historico.php'>Histórico</a> | ";
+            echo "<a href='user-new.php'>| Novo usuário</a> ";
+            echo "| Novo jogo";
+            echo " | <a href='historico.php'>Histórico</a> ";
         }
 
-        echo "<a href='user-logout.php'>Sair</a>";
+        if( is_editor() && ($_SESSION['senhaExpirada'] == false) ){
+            echo " | <a href='historico.php'>Histórico</a> ";
+        }
+
+        echo "<a href='user-logout.php'>| Sair</a>";
     }
 
     echo "</header>";
